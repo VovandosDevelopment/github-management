526ef17672a9b7b9fc2e273e8a34cdf74d2a95ac
Volodymyr Vrublevskyy <vovandos@gmail.com>
2020-12-11 17:24:22 +0200

    GitHub repos management

---
+++ repository.tf
@@ -0,0 +1,31 @@
+# The terraform resource for the repository
+resource "github_repository" "github-management" {
+  name        = "github-management"
+  description = "Terraform based repository to manage all our GutHub repositories"
+
+  private            = false
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  auto_init          = false
+  topics             = ["config", "terraform"]
+}
+
+# Set up baseline configs for the repo
+resource "github_branch_protection" "team_baseline_config" {
+  repository     = github_repository.github-management.name
+  branch         = "master"
+
+  required_status_checks {
+    # require up to date before merging
+    strict = true
+    contexts = ["atlas/mononoke/github-management", ]
+  }
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = false
+  }
+}
+
